e66092991dc95aecbebd3d33ee4d5f4c5603e375
nathan <berserk.nate@gmail.com>
2019-05-11 18:24:54 +0700

    Change Project Metadata

---
+++ app/src/main/java/id/ac/umn/AgustinusNathaniel/adapter/PokeCardAdapter.java
@@ -0,0 +1,103 @@
+package id.ac.umn.AgustinusNathaniel.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import id.ac.umn.AgustinusNathaniel.CardDetailActivity;
+import id.ac.umn.AgustinusNathaniel.R;
+import id.ac.umn.AgustinusNathaniel.model.PokeCard;
+
+public class PokeCardAdapter extends RecyclerView.Adapter<PokeCardAdapter.ViewHolder> {
+
+    public static final String KEY_NAME="name";
+    public static final String KEY_IMAGE="image";
+
+    public static final String KEY_SUPERTYPE="supertype";
+    public static final String KEY_TYPE="subtype";
+
+    public static final String KEY_SERIES="series";
+    public static final String KEY_SET="set";
+    public static final String KEY_SETCODE="setcode";
+
+    public List<PokeCard> pokeCards;
+    public Context context;
+
+    public PokeCardAdapter(List<PokeCard> pokeCards, Context context){
+        this.pokeCards = pokeCards;
+        this.context = context;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_list, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final PokeCard pokeCard = pokeCards.get(position);
+        holder.card_name.setText(pokeCard.getCardName());
+
+        Picasso.with(context)
+                .load(pokeCard.getImageUrl())
+                .fit()
+                .placeholder(R.drawable.loading_animation)
+                .into(holder.card_image);
+
+        holder.linear_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PokeCard pokeCard1 = pokeCards.get(position);
+                Intent intent = new Intent(v.getContext(), CardDetailActivity.class);
+                intent.putExtra(KEY_NAME, pokeCard1.getCardName());
+                intent.putExtra(KEY_IMAGE, pokeCard1.getImageUrlHiRes());
+                intent.putExtra(KEY_SUPERTYPE, pokeCard1.getSupertype());
+                intent.putExtra(KEY_TYPE, pokeCard1.getSubtype());
+                intent.putExtra(KEY_SERIES, pokeCard1.getSeries());
+                intent.putExtra(KEY_SET, pokeCard1.getSet());
+                intent.putExtra(KEY_SETCODE, pokeCard1.getSetCode());
+                v.getContext().startActivity(intent);
+            }
+
+        });
+
+//        holder.linear_card.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//
+//            }
+//        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return pokeCards.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView card_name;
+        ImageView card_image;
+        LinearLayout linear_card;
+
+        public ViewHolder(View cardView){
+            super(cardView);
+
+            card_name = cardView.findViewById(R.id.card_name);
+            card_image = cardView.findViewById(R.id.card_image);
+            linear_card = cardView.findViewById(R.id.linear_card);
+        }
+    }
+}
---
+++ app/src/main/java/id/ac/umn/AgustinusNathaniel/helper/LoginDatabaseHelper.java
@@ -0,0 +1,45 @@
+package id.ac.umn.AgustinusNathaniel.helper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class LoginDatabaseHelper extends SQLiteOpenHelper {
+    public static final String database_name = "credential.db";
+    public static final String table_name = "users";
+
+    private SQLiteDatabase db;
+
+    public interface TableColumns{
+        String user_username = "username";
+        String user_password = "password";
+    }
+
+    public LoginDatabaseHelper(Context context){
+        super(context, database_name, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE table " + table_name + " (username varchar primary key, password varchar, logged_in varchar)");
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TableColumns.user_username, "user");
+        contentValues.put(TableColumns.user_password, "useruser");
+        db.insert(table_name, null, contentValues);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE if exists " + table_name);
+    }
+
+    public boolean loginCheck(String username, String password){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + table_name + " WHERE username='" + username + "' AND password='" + password + "'", null);
+        if(cursor.getCount()>0) return true;
+        else return false;
+    }
+}
